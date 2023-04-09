804389a028649ae0c50d828fd385e852c5191380
OljaMatv <129103641+OljaMatv@users.noreply.github.com>
2023-04-09 14:35:45 +0300

    Update str_func.py
    Было внесено изменение

--- str_func.py
+++ str_func.py
@@ -1,7 +1,9 @@
 def capitalize_all_letters(str):
-    """Capitalizes all letters in the input string"""
+    """Capitalizes all letters in the input string!!!!"""
+     """Capitalizes all letters in the input string!!!!"""
     return str.upper()
 
 def capitalize_first_letters(str):
+    """Capitalizes the first letter of each word in the input string!!!"""
     """Capitalizes the first letter of each word in the input string!!!"""
     return str.title()
